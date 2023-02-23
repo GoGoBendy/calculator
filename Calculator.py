e30d2b4014e821ce65dff0793dd3619a416c49b7
GoGoBendy <110510326+GoGoBendy@users.noreply.github.com>
2023-02-23 13:22:02 +1100

    First update

--- Calculator.py
+++ Calculator.py
@@ -1,24 +1,25 @@
-gavlik = input("What calculation would you like? (Addition, subtraction, division, multiplication, sort) ")
-unit = gavlik.lower()
-if unit == "sort":
-    numbers = list(input("Please input the numbers you want to sort "))
-    numbers.sort()
-    print(numbers)
-else:
-    number1 = float(input("What's the first number you want to calculate?"))
-    number2 = float(input("What's the second number you want to calculate?"))
-    answer = 0
-    if unit == "addition":
-        answer = number1+number2
-        print(answer)
-    elif unit == "subtraction":
-        answer = number2-number1
-        print(answer)
-    elif unit == "division":
-        answer = number1/number2
-        print(answer)
-    elif unit == "multiplication":
-        answer = number1*number2
-        print(answer)
+while True == True:
+    gavlik = input("What calculation would you like? (Addition, subtraction, division, multiplication, sort) ")
+    unit = gavlik.lower()
+    if unit == "sort":
+        numbers = list(input("Please input the numbers you want to sort "))
+        numbers.sort()
+        print(numbers)
     else:
-        print("Error: input not recognised")
+        number1 = float(input("What's the first number you want to calculate?"))
+        number2 = float(input("What's the second number you want to calculate?"))
+        answer = 0
+        if unit == "addition":
+            answer = number1+number2
+            print(answer)
+        elif unit == "subtraction":
+            answer = number2-number1
+            print(answer)
+        elif unit == "division":
+            answer = number1/number2
+            print(answer)
+        elif unit == "multiplication":
+            answer = number1*number2
+            print(answer)
+        else:
+            print("Error: input not recognised")
